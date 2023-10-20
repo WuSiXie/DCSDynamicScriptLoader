de91beb7f4b9127748dd275805d1fd659bae68f6
物化_Pa <42517190+WuSiXie@users.noreply.github.com>
2023-10-20 23:07:59 +0800

    Add files via upload

--- ScriptLoaderInServerENV.lua
+++ ScriptLoaderInServerENV.lua
@@ -1,47 +1,45 @@
 package.path  = package.path..";.\\LuaSocket\\?.lua;"
 package.cpath = package.cpath..";.\\LuaSocket\\?.dll;"
-
-
+net.log("**************************************************************************")
+net.log(lfs.currentdir())
+--LoopTimes = 1
 local socket = require("socket")
 local coroutine = require("coroutine")
-local lfs = require("lfs")
-
+local DynamicLoaderCallbacks = {}
 -- 创建服务器套接字
-local server = socket.bind("0.0.0.0", 49527)  -- 在所有网络接口上监听端口 49527
-server:settimeout(0)  -- 设置为非阻塞模式
+local server = socket.bind("0.0.0.0", 59527) 
+server:settimeout(0) 
 
 -- 存储已连接的客户端
 local scriptLoader = {}
 scriptLoader.RefreshRate = 1.0
 scriptLoader.PasswdTimeOut = 5
---scriptLoader.Passwd = "995277" --不建议将密码写在任务脚本里
-gpasswd = ""
-dofile(lfs.currentdir().."ScriptLoaderPasswd\\passwd.lua") --请把ScriptLoaderPasswd文件夹放到游戏根目录里
-scriptLoader.Passwd = gpasswd
-gpasswd = nil
+scriptLoader.Passwd = "995277" --服务器脚本可以内置密码
 scriptLoader.clients = {}
 scriptLoader.clientsWithoutPsswd = {}
 scriptLoader.clientsWithoutPsswdInitTime = {}
-scriptLoader.Enviorment = "Mission"
+scriptLoader.Enviorment = "Server"
+--scriptLoader.Enviorment = "mission"
 
 -- 主循循环，等待连接请求
 
 local receiver = coroutine.create(function()
 	while true do
-		--trigger.action.outText("Try RecieveDATAO",5,false)
 		for i, client in ipairs(scriptLoader.clients) do
-			--trigger.action.outText("Try RecieveDATA",5,false)
+			--net.log("Try revieveDATA")
 			local data, err ,anotherDATA= client:receive(1024*4)
 			if (data or anotherDATA) then
 				local moduleId = ""
 				local message = ""
 				if (data) then
+					net.log("data:"..data)
 					--trigger.action.outText(data,5,false)
 					moduleId, message = string.match(data, "^(.-) (.+)$")
+
 				elseif(anotherDATA) then
 					if (anotherDATA ~= "") then
 						--trigger.action.outText(anotherDATA,5,false)
-						--net.log("anotherDATA:"..anotherDATA)
+						net.log("anotherDATA:"..anotherDATA)
 						moduleId, message = string.match(anotherDATA, "^(.-) (.+)$")
 					end
 				end
@@ -57,37 +55,41 @@ local receiver = coroutine.create(function()
 						client:send(scriptLoader.Enviorment)
 					end
 				end
+			else
+				net.log(err)
 			end
-			if err == "closed" then
+			if data then
+				-- 处理接收到的数据
+			elseif err == "closed" then
 				-- 客户端关闭连接
 				table.remove(scriptLoader.clients, i)
 			end
 		end
 		for i, client in ipairs(scriptLoader.clientsWithoutPsswd) do
 			local data, err ,anotherDATA= client:receive(100)
+			net.log(scriptLoader.Passwd)--debug
 			if anotherDATA == scriptLoader.Passwd then
 				client:send("Connected")
-				--trigger.action.outText("Connected",5,false)
 				table.insert(scriptLoader.clients, client)
-				--trigger.action.outText("Connected1",5,false)
 				table.remove(scriptLoader.clientsWithoutPsswd, i)
-				--trigger.action.outText("Connected2",5,false)
 				table.remove(scriptLoader.clientsWithoutPsswdInitTime, i)
-				--trigger.action.outText("Connected3",5,false)
+				net.log("Connected")
 			elseif anotherDATA ~= nil then
-				--trigger.action.outText("disconnect1",5,false)
+				net.log("Disconnect1")--debug
 				client:send("PasswdError")
 				client:close()
 				table.remove(scriptLoader.clientsWithoutPsswd, i)
 				table.remove(scriptLoader.clientsWithoutPsswdInitTime, i)
 			elseif data then
-				--trigger.action.outText("disconnect2",5,false)
 				client:send("PasswdError")
+				net.log("Disconnect2")--debug
 				client:close()
 				table.remove(scriptLoader.clientsWithoutPsswd, i)
 				table.remove(scriptLoader.clientsWithoutPsswdInitTime, i)
-			elseif timer.getTime() - scriptLoader.clientsWithoutPsswdInitTime[i] > scriptLoader.PasswdTimeOut then
-				--trigger.action.outText("disconnect3",5,false)
+			elseif DCS.getModelTime() - scriptLoader.clientsWithoutPsswdInitTime[i] > scriptLoader.PasswdTimeOut then
+				net.log("TimeOut:"..scriptLoader.clientsWithoutPsswdInitTime[i]+scriptLoader.PasswdTimeOut)
+				net.log("CurrentTime:"..DCS.getModelTime())
+				net.log("Disconnect3")--debug
 				client:close()
 				table.remove(scriptLoader.clientsWithoutPsswd, i)
 				table.remove(scriptLoader.clientsWithoutPsswdInitTime, i)
@@ -97,20 +99,24 @@ local receiver = coroutine.create(function()
 	end
 end)
 
-function mainLoop()
-    -- 检查是否有新的客户端连接
-    local client = server:accept()
-    if client then
-		client:settimeout(0)
-        table.insert(scriptLoader.clientsWithoutPsswd, client)
-        table.insert(scriptLoader.clientsWithoutPsswdInitTime, timer.getTime())--MissionENV
-    end
-	
-	coroutine.resume(receiver)
+local LastTime = DCS.getModelTime()
 
-    -- 调度下一次主循循环
-    timer.scheduleFunction(mainLoop, nil, timer.getTime() + scriptLoader.RefreshRate)  -- 1秒钟后再次调用主循环
+function DynamicLoaderCallbacks.onSimulationFrame()
+    -- 检查是否有新的客户端连接
+	if LastTime + scriptLoader.RefreshRate < DCS.getModelTime() then
+		--net.log(LoopTimes)
+		--LoopTimes = LoopTimes + 1
+        LastTime = DCS.getModelTime()
+		local client = server:accept()
+		if client then
+			client:settimeout(0)
+			table.insert(scriptLoader.clientsWithoutPsswd, client)
+			table.insert(scriptLoader.clientsWithoutPsswdInitTime, DCS.getModelTime())--ServerENV
+		end
+		
+		coroutine.resume(receiver)
+	end
+
 end
 
--- 启动主循环
-mainLoop()
+DCS.setUserCallbacks(DynamicLoaderCallbacks)
